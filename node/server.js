61152efbe4dfb4ade4286d74776d29249919067b
EberDroid <sbl@MacBook-de-Eber.local>
2016-04-23 09:00:56 -0500

    Incio de commit

--- node/server.js
+++ node/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send("Hola DYS Edición 2 ");
-});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+function decir(palabra) {
+  console.log(palabra);
+}
+
+function ejecutar(algunaFuncion, valor) {
+  algunaFuncion(valor);
+}
+
+ejecutar(decir, "Hola");
